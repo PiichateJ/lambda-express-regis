833e6159da2140c24e0d375ea4485369e7e96479
Adisorn Sakchaoren <adisorn.s@ticketmelon.com>
2017-08-16 14:19:00 +0700

    add first

---
+++ app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const doc = require('dynamodb-doc');
+const dynamo = new doc.DynamoDB();
+const jwt = require('jsonwebtoken');
+const uuidv1 = require('./lib/MelonUuid');
+
+app.db = dynamo;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+// middlewares add
+app.use('/apiwithauth', require('./middlewares/auth'));
+
+// app controllers
+app.use('/', require('./routes'));
+
+app.post('/users/regis', function(req, res) {
+  var params = {
+    TableName: 'users',
+    Item: {
+      //'id': '123e4567e89b12d3a456426655440000',
+      'birthday': new Date('1994-02-06').getTime(),
+      'created_at': new Date().getTime(),
+      'email': req.body.email,
+      'eo': [],
+      'fcm': req.body.fcm,
+      'fname': req.body.fname,
+      'gender': req.body.gender,
+      'id': '' + uuidv1(),
+      'is_active': true,
+      'jwt': 'aa',
+      'lname': req.body.lname,
+      'password': req.body.password,
+      'phone': {
+        'code': req.body.phonecode,
+        'number': req.body.phonenumber,
+      },
+      'photo': req.body.photo,
+      'role': 'user',
+      'social': [],
+      'updated_at': new Date().getTime(),
+    },
+  };
+  app.db.putItem(params, function(err, data) {
+    if (err) {
+      console.error('Unable to add item. Error JSON:', JSON.stringify(err, null, 2));
+      res.json(JSON.stringify(err, null, 2));
+    } else {
+      console.log('Added item:', uuidv1());
+      res.json(data);
+    }
+  });
+});
+module.exports = app;
---
+++ dist/lambda.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var awsServerlessExpress = require('aws-serverless-express');
+var app = require('./app');
+var server = awsServerlessExpress.createServer(app);
+exports.handler = function (event, context) {
+  return awsServerlessExpress.proxy(server, event, context);
+};
